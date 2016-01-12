aa2f03cdc5bc5da65053bba469351543e8cc9836
Rails Udemy Course <railsudemycourse1@gmail.com>
2016-01-12 18:17:00 +0000

    flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
     
     def destroy
       @article.destroy
-      flash[:notice] = "Article deleted"
+      flash[:danger] = "Article deleted"
       redirect_to articles_path
     end
     
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
        
       @article = Article.new(article_params)
       if @article.save
-        flash[:notice] = "Article was succesfully created!"
+        flash[:success] = "Article was succesfully created!"
       redirect_to article_path(@article)
         else
           render 'new'
@@ -35,7 +35,7 @@ class ArticlesController < ApplicationController
     
     def update
         if @article.update(article_params)
-          flash[:notice] = "Article was succesfully updated"
+          flash[:success] = "Article was succesfully updated"
           redirect_to article_path(@article)
         else
           render 'edit'
